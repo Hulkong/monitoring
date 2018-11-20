d3b86fa948516eb509af1496afd1c97cb869d95e
Hulkong <cdfgogo0615@naver.com>
2018-11-20 17:49:25 +0900

    Update development.js

--- config/development.js
+++ config/development.js
@@ -1,8 +1,7 @@
 const development = module.exports;
 
 development.info = {
-    // 'svcList': require('../info/TEST-SERVICE-LIST'),
-    'svcList': require('../info/SERVICE-LIST'),
+    'svcList': require('../info/TEST-SERVICE-LIST'),
     'slackInfo': require('../info/TEST-SLACK-INFO'),
     'socketIp': 'localhost',
     'httpStatus': require('../info/HTTP-STATUS')
