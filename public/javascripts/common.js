eb2c4d455e1956fd0674dcbb11f494432a2af97c
Hulkong <cdfgogo0615@naver.com>
2018-11-23 11:32:04 +0900

    Add the clock

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -39,7 +39,51 @@ const commInit = () => {
     });
     connectNodeJs();   // nodejs 서버 웹소켓 생성 및 연결
     preventHistory();   // 뒤로가기, 앞으로가기 비활성화
+    printClock();
 };
+
+/**
+ * @description 시계 만드는 함수
+ */
+const printClock = () => {
+
+    let clock = document.getElementById("clock");            // 출력할 장소 선택
+    let currentDate = new Date();                                     // 현재시간
+    let calendar = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate() // 현재 날짜
+    let amPm = 'AM'; // 초기값 AM
+    let currentHours = addZeros(currentDate.getHours(), 2);
+    let currentMinute = addZeros(currentDate.getMinutes(), 2);
+    let currentSeconds = addZeros(currentDate.getSeconds(), 2);
+
+    if (currentHours >= 12) { // 시간이 12보다 클 때 PM으로 세팅, 12를 빼줌
+        amPm = 'PM';
+        currentHours = addZeros(currentHours - 12, 2);
+    }
+
+    if (currentSeconds >= 50) {// 50초 이상일 때 색을 변환해 준다.
+        currentSeconds = '<span style="color:#de1951;">' + currentSeconds + '</span>'
+    }
+    clock.innerHTML = currentHours + ":" + currentMinute + ":" + currentSeconds + " <span>" + amPm + "</span>"; //날짜를 출력해 줌
+
+    setTimeout("printClock()", 1000);         // 1초마다 printClock() 함수 호출
+}
+
+/**
+ * @description 자릿수 맞춰주는 함수
+ * @param {*} num 현재 시 or 분 or 초
+ * @param {*} digit 맞추고 싶은 자릿수
+ * @returns 맞춰진 시 or 분 or 초
+ */
+const addZeros= (num, digit) => {
+    let zero = '';
+    num = num.toString();
+    if (num.length < digit) {
+        for (i = 0; i < digit - num.length; i++) {
+            zero += '0';
+        }
+    }
+    return zero + num;
+}
 
 /**
  * @description 뒤로가기, 앞으로가기 비활성화 하는 함수
