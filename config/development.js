a3554cbee6f014fd5072d15a05092f638910c4e8
hulkong <cdfgogo0615@naver.com>
2019-04-12 15:33:10 +0900

    upgrade lib version

--- config/development.js
+++ config/development.js
@@ -1,8 +1,8 @@
 const development = module.exports;
 
 development.info = {
-    'svcList': require('../info/TEST-SERVICE-LIST'),
-    'slackInfo': require('../info/TEST-SLACK-INFO'),
+    'svcList': require('../info/SERVICE-LIST'),
+    'slackInfo': require('../info/SLACK-INFO'),
     'socketIp': 'localhost',
     'httpStatus': require('../info/HTTP-STATUS')
 };
